8152cd408b8c8bc4154d53108a57fd0c7a0acc00
Nicolas De Mets <nico_demets@hotmail.com>
2015-11-10 10:53:29 +0100

    apres merge

--- app/src/main/java/ephec/noticeme/DBHelper.java
+++ app/src/main/java/ephec/noticeme/DBHelper.java
@@ -147,35 +147,6 @@ public class DBHelper extends SQLiteOpenHelper
         return alarm;
     }
 
-    public Alarm getAlarm(String title) {
-        String query = "Select * FROM " + TABLE_ALARMS + " WHERE " + COLUMN_TITLE + " =  \"" + title + "\"";
-
-        SQLiteDatabase db = this.getReadableDatabase();
-
-        Cursor cursor = db.rawQuery(query, null);
-
-        Alarm alarm = new Alarm();
-
-        if (cursor.moveToFirst()) {
-            cursor.moveToFirst();
-
-            alarm.setId((cursor.getInt(cursor.getColumnIndex(COlUMN_ID))));
-            alarm.setGroupId((cursor.getInt(cursor.getColumnIndex(COLUMN_GROUP_ID))));
-            alarm.setModificationDate((cursor.getString(cursor.getColumnIndex(COLUMN_MODIF_DATE))));
-            alarm.setTitle((cursor.getString(cursor.getColumnIndex(COLUMN_TITLE))));
-            alarm.setDescription((cursor.getString(cursor.getColumnIndex(COLUMN_DESCRIPTION))));
-            alarm.setLatitude(Double.parseDouble(cursor.getString(cursor.getColumnIndex(COLUMN_LATITUDE))));
-            alarm.setLongitude(Double.parseDouble(cursor.getString(cursor.getColumnIndex(COLUMN_LONGITUDE))));
-            alarm.setAlarmDate((cursor.getString(cursor.getColumnIndex(COLUMN_ALARM_DATE))));
-
-            cursor.close();
-        } else {
-            alarm = null;
-        }
-        db.close();
-        return alarm;
-    }
-
     public ArrayList<Alarm> getAllAlarm(){
 
         String query = "Select * FROM " + TABLE_ALARMS + " ORDER BY "+COLUMN_ALARM_DATE+" ASC";
@@ -208,29 +179,6 @@ public class DBHelper extends SQLiteOpenHelper
         db.close();
 
         return alarms;
-    }
-
-    public boolean deleteAlarm(String title) {
-
-        boolean result = false;
-
-        String query = "Select * FROM " + TABLE_ALARMS + " WHERE " + COLUMN_TITLE + " =  \"" + title + "\"";
-
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        Cursor cursor = db.rawQuery(query, null);
-
-        Alarm alarm = new Alarm();
-
-        if (cursor.moveToFirst()) {
-            alarm.setId((cursor.getInt(cursor.getColumnIndex(COlUMN_ID))));
-            db.delete(TABLE_ALARMS, COlUMN_ID + " = ?",
-                    new String[]{String.valueOf(alarm.getId())});
-            cursor.close();
-            result = true;
-        }
-        db.close();
-        return result;
     }
 
     public boolean deleteAlarm(String title) {
@@ -350,8 +298,7 @@ public class DBHelper extends SQLiteOpenHelper
         return alarms;
     }
 
-    public boolean alarmExist(String title)
-    {
+    public boolean alarmExist(String title) {
         boolean exist = false;
         String query = "SELECT * FROM " + TABLE_ALARMS + " WHERE " + COLUMN_TITLE + " = \"" + title + "\"";
 
